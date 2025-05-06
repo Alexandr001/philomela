5b53ae10ec95e2829b4f40aae82f5cc641082064
953_o1 <aapopov@russoutdoor.ru>
2025-05-06 08:43:59 +0300

    Добавлен docker compose

---
+++ src/Philomela.Api/wwwroot/js/site.js
@@ -0,0 +1,58 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+const form = document.getElementById('login-form');
+const button = document.getElementById('refr-button');
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    let object = {};
+    new FormData(e.currentTarget).forEach((value, key) => {
+        object[key] = value;
+    });
+
+
+    fetch('api/Authentication/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(object)
+    })
+        .then(r => {
+            if (r.ok) {
+                window.location.replace('/privacy')
+            } else {
+                alert('Неверный логин или пароль!')
+            }
+        })
+        .catch(err => {
+            alert('Не удалось выполнить авторизацию!')
+        })
+})
+
+button.addEventListener("click", e => {
+    e.preventDefault();
+
+    fetch('api/Authentication/refresh', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify({login: "admin"})
+    })
+        .then(r => {
+            if (r.ok) {
+                window.location.replace('/privacy')
+            } else {
+                alert('Не удалось обновить токены.')
+            }
+        })
+        .catch(err => {
+            alert('Не удалось выполнить авторизацию!')
+        })
+})
