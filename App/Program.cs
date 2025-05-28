65a32c71b89fde53279acdcc2ea87c40895d5966
953_o1 <aapopov@russoutdoor.ru>
2025-05-28 22:58:19 +0300

    Сделан запуск с использованием ssl

--- App/Program.cs
+++ App/Program.cs
@@ -1,11 +1,28 @@
 ﻿#pragma warning disable CA1506
 
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.EntityFrameworkCore;
 using App.Db;
 using App.Extensions;
 using App.Middlewares;
+using Microsoft.AspNetCore.Server.Kestrel.Https;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+var httpsConnectionAdapterOptions = new HttpsConnectionAdapterOptions
+{
+    SslProtocols = System.Security.Authentication.SslProtocols.Tls12,
+    ClientCertificateMode = ClientCertificateMode.AllowCertificate,
+    ServerCertificate = new X509Certificate2(
+        builder.Configuration["Certificate:Path"] ?? "./https/certificate.pfx", 
+        builder.Configuration["Certificate:Password"]
+    )
+
+};
+builder.WebHost.ConfigureKestrel(options =>
+    options.ConfigureEndpointDefaults(listenOptions =>
+        listenOptions.UseHttps(httpsConnectionAdapterOptions)));
+
 builder.Services.RegisterOptions(builder.Configuration);
 builder.Services.AddDbContext<BaseDbContext>(o =>
     o.UseNpgsql(builder.Configuration.GetConnectionString("PgSql")));
--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,10 @@
 FROM mcr.microsoft.com/dotnet/aspnet:8.0 AS base
 WORKDIR /app
-EXPOSE 5000
+
+# Создаем папку для сертификата и открываем порты
+RUN mkdir -p /app/https
+EXPOSE 80
+EXPOSE 443
 
 FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
 ARG BUILD_CONFIGURATION=Release
@@ -17,5 +21,14 @@ RUN dotnet publish "App.csproj" -c $BUILD_CONFIGURATION -o /app/publish
 
 FROM base AS final
 WORKDIR /app
+
+# Копируем опубликованное приложение
 COPY --from=publish /app/publish .
+
+# Копируем сертификат (должен лежать в ./https/certificate.pfx относительно Dockerfile)
+COPY ./App/https/certificate.pfx /app/https/certificate.pfx
+
+# Устанавливаем права (если требуется)
+RUN chmod 644 /app/https/certificate.pfx
+
 ENTRYPOINT ["dotnet", "App.dll"]
